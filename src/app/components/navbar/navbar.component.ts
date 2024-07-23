2beb07f3da9f0f29ded1747b69c6ddef9692fb82
Wiktor Opala <wiktor75300@gmail.com>
2024-07-23 18:12:32 +0200

    add new navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,25 +1,45 @@
-import { NgClass } from '@angular/common';
-import { Component, HostListener } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   showNavbar: boolean = false;
+  isMobile: boolean = false;
+  mobileMenuIsOpen: boolean = false;
+  
   previousScrollPosition: number = 0;
+  currentWindowHeight: number = 0;
+  currentWindowWidth: number = 0;
+
+  ngOnInit(): void {
+    this.onWindowResize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(): void {
+    this.currentWindowHeight = window.innerHeight;
+    this.currentWindowWidth = window.innerWidth;
+
+    if (this.currentWindowWidth <= 800) {
+      this.isMobile = true;
+    } else {
+      this.isMobile = false;
+    }
+  }
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(): void {
     const currentScrollPosition = window.scrollY;
-    const currentWindowHeight = window.innerHeight;
 
     if (
       currentScrollPosition > this.previousScrollPosition &&
-      currentScrollPosition >= currentWindowHeight
+      currentScrollPosition >= this.currentWindowHeight
     ) {
       this.showNavbar = true;
     } else if (currentScrollPosition <= this.previousScrollPosition) {
@@ -30,6 +50,6 @@ export class NavbarComponent {
   }
 
   toggleMenu(): void {
-    
+    this.mobileMenuIsOpen = !this.mobileMenuIsOpen;
   }
 }
