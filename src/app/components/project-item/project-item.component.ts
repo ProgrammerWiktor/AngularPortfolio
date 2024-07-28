d1b6a87bef78421e5b80155d44fe5fa6319561bc
Wiktor Opala <wiktor75300@gmail.com>
2024-07-28 13:38:17 +0200

    add project-item component

--- src/app/components/project-item/project-item.component.ts
+++ src/app/components/project-item/project-item.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { SkillItemComponent } from "../skill-item/skill-item.component";
 
 @Component({
   selector: 'app-project-item',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage, SkillItemComponent],
   templateUrl: './project-item.component.html',
   styleUrl: './project-item.component.scss'
 })
 export class ProjectItemComponent {
-
+  @Input() title!: string;
+  @Input() imageSrc!: string;
 }
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LanguageItemComponent } from "../language-item/language-item.component";
 import { CarouselComponent } from "../carousel/carousel.component";
+import { ProjectItemComponent } from "../project-item/project-item.component";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [LanguageItemComponent, CarouselComponent],
+  imports: [LanguageItemComponent, CarouselComponent, ProjectItemComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
