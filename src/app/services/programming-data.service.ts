9894b82005e9fc0d29276e97540e18eb02133225
Wiktor Opala <wiktor75300@gmail.com>
2024-07-28 20:54:59 +0200

    add selecting language

--- src/app/services/programming-data.service.ts
+++ src/app/services/programming-data.service.ts
@@ -1,18 +1,43 @@
 import { Injectable } from '@angular/core';
 import { ProgrammingLanguage } from '../../../public/interfaces/ProgrammingLanguage.interface';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProgrammingDataService {
-  // private data: ProgrammingLanguage[] = [];
-  private dataurl: string = 'data/ProgrammingLanguages.json';
+  private dataUrl: string = 'data/ProgrammingLanguages.json';
+  programmingData: ProgrammingLanguage[] = [];
+  selectedLanguage: string = 'All';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
+
+  loadProgrammingData(): Observable<ProgrammingLanguage[]> {
+    return this.http.get<ProgrammingLanguage[]>(this.dataUrl).pipe(
+      tap((data: ProgrammingLanguage[]) => {
+        this.programmingData = data;
+      })
+    );
+  }
 
   getProgrammingData(): Observable<ProgrammingLanguage[]> {
-    return this.http.get<ProgrammingLanguage[]>(this.dataurl);
+    if (this.programmingData.length) {
+      return of(this.programmingData);
+    } else {
+      return this.loadProgrammingData();
+    }
+  }
+
+  getSelectedItem(): string {
+    return this.selectedLanguage;
+  }
+
+  selectLanguage(language: string): void {
+    if (this.selectedLanguage === language) {
+      this.selectedLanguage = 'All';
+    } else {
+      this.selectedLanguage = language;
+    }
   }
 }
