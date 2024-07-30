f48aa760de6a0f56bd5ab338658de6641ca22654
Wiktor Opala <wiktor75300@gmail.com>
2024-07-30 20:04:46 +0200

    add contact section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { AboutMeComponent } from "./components/about-me/about-me.component";
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { SkillsComponent } from "./components/skills/skills.component";
-import { ProjectsComponent } from "./components/projects/projects.component";
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AboutMeComponent, NavbarComponent, SkillsComponent, ProjectsComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    AboutMeComponent,
+    NavbarComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  
-}
+export class AppComponent {}
